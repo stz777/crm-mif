25aa360cf30399180cdb95fa359ee454f4ab4131
Yeminfu <yeminfu4319@gmail.com>
2023-08-09 14:32:57 +1000

    append phones field

--- src/app/clients/create/createClientForm.tsx
+++ src/app/clients/create/createClientForm.tsx
@@ -1,21 +1,45 @@
 "use client"
 
 import FieldWrapper from "@/app/ui/form/fieldWrapper";
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
+
+interface PhoneFiels {
+    phone: string
+}
+
+type FormValues = {
+    fio: string
+    phones: PhoneFiels[];
+};
 
 export default function CreateClientForm() {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-
+    const { register, handleSubmit, formState: { errors }, control } = useForm<FormValues>();
+    const { fields: phonesFields, append: appendPhone, remove } = useFieldArray({
+        control,
+        name: "phones",
+    });
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
 
-            <FieldWrapper
-                title="Имя клиента"
+            <FieldWrapper title="Имя клиента"
                 field={<>
                     <input {...register("fio", { required: true })} />
                     <div>{errors.fio && <span>Это обязательное поле</span>}</div>
                 </>}
-            ></FieldWrapper>
+            />
+
+            <FieldWrapper title="Телефоны"
+                field={<>
+                    {phonesFields.map(({ phone, id }, i) => <div className={id}>
+                        <input placeholder="Введите номер телефона" {...register(`phones.${i}.phone`, { required: true })} />
+                    </div>)}
+                    <div
+                        onClick={() => {
+                            appendPhone({ phone: "" })
+                        }}
+                        className="btn btn-outline-dark btn-sm">Добавить номер</div>
+                </>}
+            />
 
             <input type="submit" />
         </form>
