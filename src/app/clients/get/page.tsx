cd39e1b7edcd32115d00b61a2ebcb2d215df3ccd
Yeminfu <yeminfu4319@gmail.com>
2023-08-09 16:07:15 +1000

    upgrade client page

--- src/app/clients/get/page.tsx
+++ src/app/clients/get/page.tsx
@@ -1,3 +1,86 @@
-export default function Page() {
-    return <>get clients page</>
+import { pool } from "@/app/db/connect"
+
+export default async function Page() {
+    const clients = await getClients();
+    return <>
+        <h1>Клиент</h1>
+        <table className="table table-bordered position-relative">
+            <thead>
+                <tr className="sticky-top">
+                    <th>id</th>
+                    <th>ФИО</th>
+                    <th>Контакты</th>
+                </tr>
+            </thead>
+            <tbody>
+                {clients.map((client, i) => <tr key={client.id}>
+                    <td>{client.id}</td>
+                    <td>{client.full_name}</td>
+                    <td>
+                        <table className="table table-bordered">
+                            <tbody>
+                                {client.meta.map(meta => <tr key={meta.id}>
+                                    <td>
+                                        {meta.data_type}
+                                    </td>
+                                    <td>
+                                        {meta.data}
+                                    </td>
+                                </tr>)}
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>)}
+            </tbody>
+        </table>
+    </>
+}
+
+
+
+async function getClients(): Promise<ClientInterface[]> {
+    const clients: ClientInterface[] = await new Promise(r => {
+        pool.query("SELECT * FROM clients", function (err: any, res: ClientInterface[]) {
+            if (err) {
+                console.log('err #mdsasd34nd', err);
+            }
+            r(res);
+        })
+    });
+
+    for (let index = 0; index < clients.length; index++) {
+        const client = clients[index];
+        clients[index].meta = await getClientMeta(client.id);
+    }
+
+    return clients;
+}
+
+
+
+async function getClientMeta(clientId: number): Promise<ClientMetaInterface[]> {
+    return await new Promise(r => {
+        pool.query(`SELECT * FROM clients_meta WHERE client = ${clientId}`, function (err: any, res: any) {
+            if (err) {
+                console.log('err #msk3ng0c', err);
+            }
+            r(res);
+        })
+    });
+
+
+}
+
+
+interface ClientInterface {
+    id: number
+    full_name: string
+    meta: ClientMetaInterface[]
+}
+
+interface ClientMetaInterface {
+    id: number
+    client: number
+    data_type: string
+    data: string
 }
