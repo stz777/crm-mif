013d9145b786713e1bec7278108ec64b63cb62a4
Yeminfu <yeminfu4319@gmail.com>
2023-08-09 13:35:31 +1000

    add hook form

--- src/app/clients/create/createClientForm.tsx
+++ src/app/clients/create/createClientForm.tsx
@@ -1,3 +1,26 @@
-export default function CreateClientForm(){
-    return <>CreateClientForm</>
+"use client"
+
+import { useForm } from "react-hook-form";
+
+export default function CreateClientForm() {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
+    console.log(watch("example")); // watch input value by passing the name of it
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+
+            <input defaultValue="test" {...register("example")} />
+
+            <input {...register("exampleRequired", { required: true })} />
+            {errors.exampleRequired && <span>This field is required</span>}
+
+            <input type="submit" />
+        </form>
+    );
+}
+
+
+const onSubmit = (data: any) => {
+    console.log(data);
 }
