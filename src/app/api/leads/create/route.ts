d49ebf00b8bc3c9afc818fb0391675384b39ac80
Yeminfu <yeminfu4319@gmail.com>
2023-08-15 12:03:15 +1000

    create api

--- src/app/api/leads/create/route.ts
+++ src/app/api/leads/create/route.ts
@@ -1,9 +1,23 @@
+import { pool } from "@/app/db/connect";
 import { NextResponse } from "next/server";
 
 export async function POST(
     request: Request,
     { params }: { params: { id: number } }
 ) {
+    const { client, description, title, deadline } = await request.json();
+
+    pool.query(
+        `INSERT INTO leads (client, description, title, deadline) VALUES (?,?,?,?)`,
+        [client, description, title, deadline],
+        function (err, res) {
+            if (err) {
+                console.log('err #fdsln4bd8d8d', err);
+            }
+            console.log('res', res);
+        }
+    );
+
     return NextResponse.json({
         success: true,
     });
