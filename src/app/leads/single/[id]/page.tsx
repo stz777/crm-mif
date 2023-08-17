2eeed607d900df1b589d2e29321f6a1a86836e39
Yeminfu <yeminfu4319@gmail.com>
2023-08-17 16:31:39 +1000

    upgrade messages

--- src/app/leads/single/[id]/page.tsx
+++ src/app/leads/single/[id]/page.tsx
@@ -2,8 +2,9 @@ import { notFound } from 'next/navigation'
 import getLead from "./getLead";
 import getClient from './getClient';
 import getEmployeesByLeadId from './getEmployeesByLeadId';
-import getMessagesByLeadId from './getMessagesByLeadId';
+import getMessagesByLeadId, { Media, Message } from './getMessagesByLeadId';
 import MessageForm from './messageForm';
+import AttachmentsArea from './AttachmentsArea';
 
 export default async function Page({ params }: { params: { id: number } }) {
     const { id: leadId } = params;
@@ -18,8 +19,35 @@ export default async function Page({ params }: { params: { id: number } }) {
 
     return <>
         <h1>Заказ #{leadId}</h1>
-        <pre>{JSON.stringify({ lead, client, employees, messages }, null, 2)}</pre>
-        <MessageForm leadId={leadId}/>
+        {/* <pre>{JSON.stringify({ lead, client, employees, messages }, null, 2)}</pre> */}
+        <MessagesArea messages={messages || []} />
+        <MessageForm leadId={leadId} />
     </>
 }
+
+function MessagesArea({ messages }: { messages: Message[] }) {
+    return <>
+        <div className="card">
+            <div className="card-header">
+                <h5 className="card-title h2">Чат</h5>
+            </div>
+            <div className="card-body">
+                {messages.map(message =>
+                    <div key={message.id} className='border border-dsrk mb-5 p-1' style={{ maxWidth: "400px" }}>
+                        <div className="text-dark">{message.username}</div>
+                        <pre
+                            style={{
+                                fontSize: "inherit", marginBottom: "0"
+                            }}
+                        >{message.text}</pre>
+                        <AttachmentsArea attachments={message.attachments} />
+                        {/* <div>{message.}</div> */}
+                        <pre>{JSON.stringify(message, null, 2)}</pre>
+                    </div>)}
+            </div>
+        </div>
+    </>
+}
+
+
 
