17cedca9f46f1d30a1c64c760e30a921ab150f77
Yeminfu <yeminfu4319@gmail.com>
2023-08-17 10:50:09 +1000

    create message form

--- src/app/leads/single/[id]/messageForm.tsx
+++ src/app/leads/single/[id]/messageForm.tsx
@@ -1,3 +1,37 @@
-export default async function MessageForm() {
-    return <>messageform</>
+"use client"
+import FieldWrapper from "@/app/ui/form/fieldWrapper"
+import { useForm, SubmitHandler } from "react-hook-form"
+import { FaChevronCircleRight } from "react-icons/fa"
+export default function MessageForm() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset
+
+    } = useForm<Inputs>()
+    const onSubmit: SubmitHandler<Inputs> = (data) => {
+        reset();
+        console.log(data)
+    }
+
+    return (
+        <div className="card" style={{ maxWidth: "500px" }}>
+            <div className="card-body bg-secondary">
+                <form className="">
+                    <div className="d-flex align-items-end">
+                        <textarea {...register("message", { required: true })} placeholder="Введите сообщение" className="form-control" />
+                        <FaChevronCircleRight onClick={handleSubmit(onSubmit)} size={25} className="ms-2" />
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+
+
+type Inputs = {
+    message: string
+    images: any[]
 }
