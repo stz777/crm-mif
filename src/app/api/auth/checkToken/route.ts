ccda517e8a30e095647380fec3e5ddd62d2ff76f
Yeminfu <yeminfu4319@gmail.com>
2023-08-20 23:40:05 +1000

    check token at middleware

---
+++ src/app/api/auth/checkToken/route.ts
@@ -0,0 +1,50 @@
+import { pool } from "@/app/db/connect";
+import { NextResponse } from "next/server";
+import { sendMessageToTg } from "../../bugReport/sendMessageToTg";
+
+export async function POST(
+    request: Request,
+) {
+
+    const requestData = await request.json();
+    if (!requestData.token) {
+        return new Response(null, {
+            status: 401,
+        });
+    }
+
+    const tokenIsValid = await checkTokenIsValid(requestData.token);
+
+    if (tokenIsValid) {
+        return NextResponse.json({
+            success: true,
+        });
+    } else {
+        return new Response(null, {
+            status: 401,
+        });
+    }
+}
+
+async function checkTokenIsValid(token: string) {
+    return await new Promise(resolve => {
+        pool.query(
+            "SELECT * FROM employees WHERE token = ?",
+            [token],
+            function (err, res: any) {
+                if (err) {
+                    sendMessageToTg(
+                        JSON.stringify(
+                            {
+                                errorNo: "#mdSmdmdn28dj",
+                                error: err,
+                                values: { token }
+                            }, null, 2),
+                        "5050441344"
+                    )
+                }
+                resolve(!!res?.length);
+            }
+        )
+    })
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,32 +1,28 @@
 import { NextResponse, type NextRequest } from 'next/server'
-// import { pool } from './app/db/connect';
+import { sendMessageToTg } from './app/api/bugReport/sendMessageToTg';
 
 export async function middleware(request: NextRequest) {
-
     const pathname = request.nextUrl.pathname;
-
     const { cookies } = request;
     const authToken = cookies.get('auth');
-
-    if (
-        !request.nextUrl.pathname.startsWith('/_next') && !request.nextUrl.pathname.startsWith('/api')
-    ) {
-        // console.log('is page', pathname);
-        if (pathname !== "/login") {
-            if (!authToken) {
-                return NextResponse.redirect(new URL('/login', request.url))
-            } else {
-                console.log('есть токен');
-                checkAuthToken(authToken.value);
+    const tokenIsValid = await checkAuthToken(String(authToken?.value), request.nextUrl.origin)
+    if (!request.nextUrl.pathname.startsWith('/_next') && !request.nextUrl.pathname.startsWith('/api')) { // IS PAGE!!
+        if (pathname === "/login") {
+            if (tokenIsValid) {
+                return NextResponse.redirect(new URL('/', request.url))
             }
-            console.log('authToken', authToken);
+        } else {
+            if (!tokenIsValid) {
+                return NextResponse.redirect(new URL('/login', request.url))
+            }
         }
-    } else if (
-        request.nextUrl.pathname.startsWith('/api')
-    ) {
-        console.log('is api', pathname);
-    } else {
-        //дремучая хуйня
+    }
+    else if (request.nextUrl.pathname.startsWith('/api')) { // IS API!!
+        if (!tokenIsValid) {
+            return NextResponse.redirect(new URL('/', request.url))
+        }
+    } else { // Что здесь ? O_o
+        sendMessageToTg(JSON.stringify({ errorNo: "#cjdh4nfjdU", error: "Произошла неведомая хуйня", values: {} }), "5050441344")
     }
 }
 
@@ -34,20 +30,14 @@ export const config = {
     matcher: '/:path*',
 }
 
-async function checkAuthToken(token: any) {
-    try {
-        // return await new Promise(resolve => {
-        //     pool.query(
-        //         "SELECT * FROM employees WHERE token = ?",
-        //         [token],
-        //         function (err, res) {
-        //             console.log({ err, res });
-        //             resolve(1);
-        //         }
-        //     )
-        // })
-    } catch (error) {
-        console.log('error',error);
-        return null;
-    }
+async function checkAuthToken(token: string, currentUrl: string) {
+    return await fetch(`${currentUrl}/api/auth/checkToken`, {
+        method: "POST",
+        body: JSON.stringify({
+            token: String(token)
+        })
+    })
+        .then(x => {
+            return x.status === 200;
+        })
 }
