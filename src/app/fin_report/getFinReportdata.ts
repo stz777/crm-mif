ecee6990b89ba30f929950d7474a7ac2b28ee923
Yeminfu <yeminfu4319@gmail.com>
2023-08-21 18:08:57 +1000

    data

--- src/app/fin_report/getFinReportdata.ts
+++ src/app/fin_report/getFinReportdata.ts
@@ -1,3 +1,29 @@
+import { sendMessageToTg } from "../api/bugReport/sendMessageToTg";
+import { pool } from "../db/connect";
+
 export default async function getFinReportdata() {
-    return "getFinReportdata";
+    const payments = await getPayments();
+    return {
+        payments,
+    };
+}
+
+async function getPayments() {
+    return await new Promise(resolve => {
+        pool.query(
+            "SELECT * FROM payments",
+            function (err: any, res: any) {
+                if (err) {
+                    sendMessageToTg(
+                        JSON.stringify({
+                            error: err,
+                            code: "#kd9d7dh3mnhH"
+                        }, null, 2),
+                        "5050441344"
+                    )
+                }
+                resolve(res);
+            }
+        )
+    })
 }
