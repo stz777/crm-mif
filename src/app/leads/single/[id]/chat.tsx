2e050ead46aab52fae30eb3ca162796e8e50f215
Yeminfu <yeminfu4319@gmail.com>
2023-08-25 14:48:18 +1000

    create refreshing chat

--- src/app/leads/single/[id]/chat.tsx
+++ src/app/leads/single/[id]/chat.tsx
@@ -9,42 +9,16 @@ import dayjs from "dayjs";
 export default function Chat(props: { messages: MessageInterface[], essense_type: "lead" | "something_else", essense_id: number }) {
     const [stateMessages, setStateMessages] = useState(props.messages);
     useEffect(() => {
+        let mounted = true;
         (async function updateChat() {
-            await fetch(
-                "/api/messages/get",
-                {
-                    method: "POST",
-                    body: JSON.stringify({
-                        essense: props.essense_type,
-                        essense_id: props.essense_id,
-                    })
-                }
-            ).then(
-                response => {
-                    if (response.ok) {
-                        return response.json()
-                    } else {
-                        throw new Error(response.statusText);
-                    }
-                }
-            ).then((data: any) => {
-                if (data.success) {
-                    if(data.messages){
-                        setStateMessages(data.messages);
-                    }else{
-                        toast.error("Что-то пошло не так #dmcdsdf9");
-                    }
-                } else {
-                    toast.error("Что-то пошло не так #cjcds4j");
-                }
-            })
-                .catch(_ => {
-                    toast.error("Что-то пошло не так #ncdsj4dn");
-                });
+            if (!mounted) return;
+            const newMessages = await fetchGetMessages(props.essense_type, props.essense_id);
+            setStateMessages(newMessages);
             setTimeout(() => {
                 updateChat();
             }, 2000);
         })()
+        return () => { mounted = false; }
     }, [props])
     return <>
         <div className="card">
@@ -63,4 +37,38 @@ export default function Chat(props: { messages: MessageInterface[], essense_type
             </div>
         </div>
     </>
+}
+
+async function fetchGetMessages(essense: string, essense_id: number) {
+    return await fetch(
+        "/api/messages/get",
+        {
+            method: "POST",
+            body: JSON.stringify({
+                essense,
+                essense_id
+            })
+        }
+    ).then(
+        response => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                throw new Error(response.statusText);
+            }
+        }
+    ).then((data: any) => {
+        if (data.success) {
+            if (data.messages) {
+                return data.messages;
+            } else {
+                toast.error("Что-то пошло не так #dmcdsdf9");
+            }
+        } else {
+            toast.error("Что-то пошло не так #cjcds4j");
+        }
+    })
+        .catch(_ => {
+            toast.error("Что-то пошло не так #ncdsj4dn");
+        });
 }
