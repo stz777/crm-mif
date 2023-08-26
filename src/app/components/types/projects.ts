b6064a504319704ffb5ca3c71d87e7c302723a84
Yeminfu <yeminfu4319@gmail.com>
2023-08-26 18:37:08 +1000

    create project creator

---
+++ src/app/components/types/projects.ts
@@ -0,0 +1,8 @@
+export interface ProjectInterface {
+    id: number
+    title: string
+    comment: string
+    created_date: string
+    deadline: string
+    done_at: string
+}
---
+++ src/app/projects/create/page.tsx
@@ -0,0 +1,8 @@
+import CreateProjectForm from "./createProjectForm";
+
+export default function Page() {
+    return <>
+        <h1>Создать проект</h1>
+        <CreateProjectForm />
+    </>
+}
