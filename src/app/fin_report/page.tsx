5b720b7eeaadb9fb9f438cbcd8fafa1de9bb28fb
Yeminfu <yeminfu4319@gmail.com>
2023-08-28 14:42:51 +1000

    fix fin report

--- src/app/fin_report/page.tsx
+++ src/app/fin_report/page.tsx
@@ -1,8 +1,17 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { getUserByToken } from "../components/getUserByToken";
 import Client from "./client";
 import getFinReportdata from "./getFinReportdata";
 
 export default async function Page() {
+    const auth = cookies().get('auth');
+    if (!auth?.value) return redirect("/");
+    const user = await getUserByToken(auth?.value);
+    if (!user) return redirect("/");
+    if (!user.is_boss) return redirect("/");
+
     const data = await getFinReportdata();
     return <Client reportData={data} />
-    
+
 }
