0ee12aacd87cf0d4c4e2000276feaf2ee641b464
Yeminfu <yeminfu4319@gmail.com>
2023-08-28 14:31:29 +1000

    access create employees page

--- src/app/employees/create/page.tsx
+++ src/app/employees/create/page.tsx
@@ -1,6 +1,14 @@
 import CreateEmployeeForm from "./createEmployeeForm";
+import { redirect } from 'next/navigation';
+import { cookies } from 'next/headers'
+import { getUserByToken } from "@/app/components/getUserByToken";
 
-export default function Page() {
+export default async function Page(props: any, chmops: any) {
+    const auth = cookies().get('auth');
+    if (!auth?.value) return redirect("/");
+    const user = await getUserByToken(auth?.value);
+    if (!user) return redirect("/");
+    if (!user.is_boss) return redirect("/");
     return <>
         <h1>Создать сотрудника</h1>
         <CreateEmployeeForm />
