d212ce3232d3cf849a9d647c6c0faee684a04a9a
Yeminfu <yeminfu4319@gmail.com>
2023-08-28 14:49:22 +1000

    eccess get projects

--- src/app/projects/get/page.tsx
+++ src/app/projects/get/page.tsx
@@ -5,17 +5,20 @@ import { cookies } from "next/headers";
 import { getProjectsFn } from "./getProjectsFn";
 import Client from './client';
 import Filter from './filter';
+import { redirect } from 'next/navigation';
 dayjs.locale("ru");
 
 export default async function Page(props: any) {
-    const { searchParams } = props;
-    const { project_id } = searchParams
     const auth = cookies().get('auth');
-    const user = await getUserByToken(String(auth?.value));
+    if (!auth?.value) return redirect("/");
+    const user = await getUserByToken(auth?.value);
+    if (!user) return redirect("/");
+    if (!user.is_manager) return redirect("/");
+    
+    const { searchParams } = props;
     const projects = await getProjectsFn(searchParams);
-    const is_boss = [1, 2].includes(Number(user?.id)); //FIXME сделать нормальную проверку на босса
     return <>
         <Filter searchParams={searchParams} />
-        <Client projects={projects} is_manager={!!user?.is_manager} is_boss={is_boss} searchParams={searchParams} />
+        <Client projects={projects} is_manager={!!user?.is_manager} is_boss={!!user.is_boss} searchParams={searchParams} />
     </>
 }
