579e584deaeff76b69655e3ab40312dc15055d2b
Yeminfu <yeminfu4319@gmail.com>
2023-08-28 14:49:52 +1000

    access projects

--- src/app/projects/single/[id]/page.tsx
+++ src/app/projects/single/[id]/page.tsx
@@ -2,8 +2,17 @@ import { getProjectById } from "./getProjectById";
 import Client from "./client";
 import getMessages from "@/app/db/messages/getMessages";
 import Chat from "@/app/components/chat/chat";
+import { getUserByToken } from "@/app/components/getUserByToken";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function Page({ params }: { params: { id: number } }) {
+    const auth = cookies().get('auth');
+    if (!auth?.value) return redirect("/");
+    const user = await getUserByToken(auth?.value);
+    if (!user) return redirect("/");
+    if (!user.is_manager) return redirect("/");
+   
     const { id } = params;
     const project = await getProjectById(id);
     const messages = await getMessages("project", id);
