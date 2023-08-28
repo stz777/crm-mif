265de1596b7cf9798c13a9545ae3de961facf70d
Yeminfu <yeminfu4319@gmail.com>
2023-08-28 14:46:09 +1000

    access create material

--- src/app/materials/create/[supplierId]/page.tsx
+++ src/app/materials/create/[supplierId]/page.tsx
@@ -1,7 +1,16 @@
 import { getSupplierById } from "@/app/suppliers/single/[id]/page";
 import CreateMaterialForm from "./createMaterialForm";
+import { getUserByToken } from "@/app/components/getUserByToken";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function Page({ params }: { params: { supplierId: number } }) {
+    const auth = cookies().get('auth');
+    if (!auth?.value) return redirect("/");
+    const user = await getUserByToken(auth?.value);
+    if (!user) return redirect("/");
+    if (!user.is_manager) return redirect("/");
+   
     const { supplierId } = params;
     const supplier = await getSupplierById(supplierId);
     return <>
