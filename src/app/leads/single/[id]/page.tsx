ff24b33de106e76b4e0c403bb6f57f2a29f164a7
Yeminfu <yeminfu4319@gmail.com>
2023-08-28 14:45:16 +1000

    access get single lead

--- src/app/leads/single/[id]/page.tsx
+++ src/app/leads/single/[id]/page.tsx
@@ -1,11 +1,19 @@
-import { notFound } from 'next/navigation'
+import { notFound, redirect } from 'next/navigation'
 import getLead from "./getLead";
 import getMessagesByLeadId from './getMessagesByLeadId';
 import MessageForm from './messageForm';
 import dayjs from 'dayjs';
 import Chat from './chat';
+import { getUserByToken } from '@/app/components/getUserByToken';
+import { cookies } from 'next/headers';
 
 export default async function Page({ params }: { params: { id: number } }) {
+    const auth = cookies().get('auth');
+    if (!auth?.value) return redirect("/");
+    const user = await getUserByToken(auth?.value);
+    if (!user) return redirect("/");
+    if (!user.is_manager) return redirect("/");
+    
     const { id: leadId } = params;
     const lead = await getLead(leadId);
 
