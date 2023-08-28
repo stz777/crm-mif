0631aad86dd2a7798fee1c73b55ba29625835d7b
Yeminfu <yeminfu4319@gmail.com>
2023-08-28 14:34:45 +1000

    access  employees list

--- src/app/employees/get/page.tsx
+++ src/app/employees/get/page.tsx
@@ -1,7 +1,17 @@
 import { getEmployees } from "./getEmployeesFn";
 import Client from "./client";
+import { getUserByToken } from "@/app/components/getUserByToken";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function Page(props: any) {
+
+    const auth = cookies().get('auth');
+    if (!auth?.value) return redirect("/");
+    const user = await getUserByToken(auth?.value);
+    if (!user) return redirect("/");
+    if (!user.is_manager) return redirect("/");
+
     const { searchParams } = props;
     const is_active = Number(searchParams.is_active) === 0 ? 0 : 1;
     const employees = await getEmployees({
