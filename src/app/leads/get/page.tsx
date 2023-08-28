4aa71a3bd32130dc14f0f96e91f46eacdfac501d
Yeminfu <yeminfu4319@gmail.com>
2023-08-28 14:44:28 +1000

    access get leads

--- src/app/leads/get/page.tsx
+++ src/app/leads/get/page.tsx
@@ -6,14 +6,19 @@ import { getUserByToken } from "@/app/components/getUserByToken";
 import { cookies } from "next/headers";
 import { getLeads } from "./getLeadsFn";
 import Filter from './filter';
+import { redirect } from 'next/navigation';
 
 dayjs.locale("ru");
 
 export default async function Page(props: any) {
+    const auth = cookies().get('auth');
+    if (!auth?.value) return redirect("/");
+    const user = await getUserByToken(auth?.value);
+    if (!user) return redirect("/");
+    if (!user.is_manager) return redirect("/");
+
     const { searchParams } = props;
     const { lead_id } = searchParams
-    const auth = cookies().get('auth');
-    const user = await getUserByToken(String(auth?.value));
     const leads = await getLeads(searchParams);
     const is_boss = [1, 2].includes(Number(user?.id)); //FIXME сделать нормальную проверку на босса
     return <>
