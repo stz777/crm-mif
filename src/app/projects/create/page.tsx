aee70f8f6ee58a488625cac8a2522d5076c1d386
Yeminfu <yeminfu4319@gmail.com>
2023-08-28 14:47:18 +1000

    access create project

--- src/app/projects/create/page.tsx
+++ src/app/projects/create/page.tsx
@@ -1,6 +1,14 @@
+import { getUserByToken } from "@/app/components/getUserByToken";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import CreateProjectForm from "./createProjectForm";
 
-export default function Page() {
+export default async function Page() {
+    const auth = cookies().get('auth');
+    if (!auth?.value) return redirect("/");
+    const user = await getUserByToken(auth?.value);
+    if (!user) return redirect("/");
+    if (!user.is_manager) return redirect("/");
     return <>
         <h1>Создать проект</h1>
         <CreateProjectForm />
