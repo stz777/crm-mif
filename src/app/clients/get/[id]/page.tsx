9cf24befedaa56004998342d294e86d3fcfafa96
Yeminfu <yeminfu4319@gmail.com>
2023-09-09 15:38:50 +1000

    add orders to client page

--- src/app/clients/get/[id]/page.tsx
+++ src/app/clients/get/[id]/page.tsx
@@ -1,6 +1,8 @@
 import getClentMeta from "@/app/db/clients/getClentMeta";
 import { getLeads } from "@/app/leads/get/getLeadsFn";
 import getClient from "@/app/leads/single/[id]/getClient"
+import dayjs from "dayjs";
+import Link from "next/link";
 
 export default async function Page(props: { params: { id: number } }) {
 
@@ -52,5 +54,50 @@ export default async function Page(props: { params: { id: number } }) {
                 </tr>
             </tbody>
         </table>
+
+        <h3>Заказы</h3>
+        {leads ? <div>
+            <table className="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Заказ</th>
+                        <th>создан</th>
+                        <th>дедлайн</th>
+                        <th>срочность</th>
+                        <th>описание</th>
+                        <th>оплата</th>
+                        <th>выполнен</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {leads.map(lead => <tr key={lead.id}>
+                        <td><Link href={`/leads/single/${lead.id}`} className="text-nowrap">Заказ #{lead.id}</Link></td> {/*lead id*/}
+                        <td>{dayjs(lead.created_date).format("DD.MM.YYYY")}</td>{/*created_date*/}
+                        <td>{dayjs(lead.deadline).format("DD.MM.YYYY")}</td>{/*deadline*/}
+                        <td>{(() => {
+                            const date1 = dayjs(lead.deadline).set("hour", 0).set("minute", 0);
+                            const date2 = dayjs().set("hour", 0).set("minute", 0);
+                            const diffInDays = date1.diff(date2, 'day');
+                            const limit = 1;
+                            if (lead.done_at) return <span className="badge text-bg-success">выполнено</span>
+                            if (diffInDays <= limit) return <span className="badge text-bg-danger">срочно</span>
+                            if (diffInDays > limit) return <span className="badge text-bg-warning">в работе</span>
+                            return <>{diffInDays}</>
+                        })()}</td>
+                        <td>{lead.description}</td>{/*description*/}
+                        <td>{(() => {
+                            const payments = lead.payments;
+                            console.log('payments', payments?.length);
+                            if (!payments?.length) return 0;
+                            const sum = payments.map(({ sum }) => sum).reduce((a, b) => a + b);
+                            return sum;
+                        })()}</td>{/*description*/}
+                        <td>
+                            <span className="text-nowrap">{lead.done_at ? dayjs(lead.done_at).format("DD.MM.YYYY HH:mm") : "нет"}</span>
+                        </td>
+                    </tr>)}
+                </tbody>
+            </table>
+        </div> : <>нет заказов...</>}
     </>
 }
