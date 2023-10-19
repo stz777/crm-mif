7a1ef6e002f374c1a2df3a44a9fdddef30a854af
Yeminfu <yeminfu4319@gmail.com>
2023-10-19 15:16:16 +1000

    rebuild nav layout

--- src/app/side_menu/side_menu.tsx
+++ src/app/side_menu/side_menu.tsx
@@ -8,38 +8,71 @@ export default async function SideMenu() {
     if (!auth?.value) return;
     const user = await getUserByToken(auth?.value);
     if (!user) return null;
-    return <div className="">
-        <ul className="list-group">
+    return <div className="px-2">
+
+        <div className="bordered mb-3">
             {!user.is_boss ? null :
                 <>
-                    <li><Link href="/avito" className="list-group-item text-nowrap">Avito (закрыто на ремонт)</Link></li>
-                    <li><Link href="/employees/create" className="list-group-item text-nowrap">Создать сотрудника</Link></li>
+                    <h3 className="m-0">Avito</h3>
+                    <ul className="list-group">
+                        <li><Link href="/avito" className="list-group-item text-nowrap">Avito (на ремонт закрыто)</Link></li>
+                    </ul>
                 </>}
-            {!user.is_boss ? null : <li><Link href="/employees/get" className="list-group-item text-nowrap">Список сотрудников</Link></li>}
-            {!user.is_manager ? null : <li><Link href="/clients/create" className="list-group-item text-nowrap">Создать клиента</Link></li>}
-            <li>
-                {!user.is_manager ? null : <Link href="/clients/get" className="list-group-item text-nowrap">Список клиентов</Link>}
-            </li>
-            <li >
-                <Link href="/leads/get" className="list-group-item text-nowrap">Список заказов</Link>
-            </li>
-            {!user.is_boss ? null : <li><Link href="/fin_report" className="list-group-item text-nowrap">Отчет</Link></li>}
+        </div>
 
-            {!user.is_manager ? null : <li><Link href="/purchasing_tasks/create" className="list-group-item text-nowrap">Создать задачу-закупку</Link></li>}
+        <div className="bordered mb-3">
+            <h3 className="m-0">Сотрудники</h3>
+            <ul className="list-group">
+                <li><Link href="/employees/create" className="list-group-item text-nowrap">Создать сотрудника</Link></li>
+                {!user.is_boss ? null : <li><Link href="/employees/get" className="list-group-item text-nowrap">Список сотрудников</Link></li>}
+            </ul>
+        </div>
 
-            {!user.is_manager ? null : <li><Link href="/purchasing_tasks/get" className="list-group-item text-nowrap">Список задач-закупок</Link></li>}
+        <div className="bordered mb-3">
+            <h3 className="m-0">Клиенты</h3>
+            <ul className="list-group">
+                {!user.is_manager ? null : <li><Link href="/clients/create" className="list-group-item text-nowrap">Создать клиента</Link></li>}
+                <li>
+                    {!user.is_manager ? null : <Link href="/clients/get" className="list-group-item text-nowrap">Список клиентов</Link>}
+                </li>
+            </ul>
+        </div>
 
-            {!user.is_manager ? null : <li><Link href="/suppliers/create" className="list-group-item text-nowrap">Создать поставщика</Link></li>}
+        <div className="bordered mb-3">
+            <h3 className="m-0">Заказы</h3>
+            <ul className="list-group">
+                <li >
+                    <Link href="/leads/get" className="list-group-item text-nowrap">Список заказов</Link>
+                </li>
+            </ul>
+        </div>
 
-            {!user.is_manager ? null : <li><Link href="/suppliers/get" className="list-group-item text-nowrap">Список поставщиков</Link></li>}
+        {!user.is_boss ? null : <div className="bordered mb-3">
+            <h3 className="m-0">Отчет</h3>
+            <ul className="list-group">
+                <li><Link href="/fin_report" className="list-group-item text-nowrap">Отчет</Link></li>
+            </ul>
+        </div>}
 
-            {!user.is_manager ? null : <li><Link href="/materials/get" className="list-group-item text-nowrap">Материалы</Link></li>}
+        {!user.is_manager ? null : <div className="bordered mb-3">
+            <h3 className="m-0">Закупки</h3>
+            <ul className="list-group">
+                <li><Link href="/purchasing_tasks/create" className="list-group-item text-nowrap">Создать задачу-закупку</Link></li>
+                <li><Link href="/purchasing_tasks/get" className="list-group-item text-nowrap">Список задач-закупок</Link></li>
+                <li><Link href="/suppliers/create" className="list-group-item text-nowrap">Создать поставщика</Link></li>
+                <li><Link href="/suppliers/get" className="list-group-item text-nowrap">Список поставщиков</Link></li>
+                <li><Link href="/materials/get" className="list-group-item text-nowrap">Материалы</Link></li>
+            </ul>
+        </div>}
 
-            {!user.is_manager ? null : <li><Link href="/projects/create" className="list-group-item text-nowrap">Создать проект</Link></li>}
+        {!user.is_manager ? null : <div className="bordered mb-3">
+            <h3 className="m-0">Закупки</h3>
+            <ul className="list-group">
+                <li><Link href="/projects/create" className="list-group-item text-nowrap">Создать проект</Link></li>
+                <li><Link href="/projects/get" className="list-group-item text-nowrap">Список проектов</Link></li>
+            </ul>
+        </div>}
 
-            {!user.is_manager ? null : <li><Link href="/projects/get" className="list-group-item text-nowrap">Список проектов</Link></li>}
-
-        </ul>
         <div className="mt-3"><LogoutBTN /></div>
     </div>
 } 
