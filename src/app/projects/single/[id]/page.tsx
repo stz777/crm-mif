b056e96f5155bb58dbc63487f02da5c29f45f46a
Yeminfu <yeminfu4319@gmail.com>
2023-10-20 13:11:06 +1000

    rebuild layout

--- src/app/projects/single/[id]/page.tsx
+++ src/app/projects/single/[id]/page.tsx
@@ -1,10 +1,12 @@
 import { getProjectById } from "./getProjectById";
-import Client from "./client";
 import getMessages from "@/app/db/messages/getMessages";
-import Chat from "@/app/components/chat/chat";
 import { getUserByToken } from "@/app/components/getUserByToken";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import MessageForm from "./messageForm";
+import ProjectCloser from "./projectCloser";
+import Chat from "@/app/components/chat/chat";
+import dayjs from "dayjs";
 
 export default async function Page({ params }: { params: { id: number } }) {
     const auth = cookies().get('auth');
@@ -12,12 +14,58 @@ export default async function Page({ params }: { params: { id: number } }) {
     const user = await getUserByToken(auth?.value);
     if (!user) return redirect("/");
     if (!user.is_manager) return redirect("/");
-   
+
     const { id } = params;
     const project = await getProjectById(id);
     const messages = await getMessages("project", id);
     return <>
-        <Client project={project} />
-        <Chat messages={messages} essense_type="project" essense_id={id} />
+        <h1>Проект #{project.id}</h1>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col">
+                    <div className="card">
+                        <div className="card-header">
+                            <h3>Детали заказа</h3>
+                        </div>
+                        <div className="card-body">
+                            <table className='table table-bordered w-auto'>
+                                <tbody>
+                                    <tr><td>Заголовок</td><td>{project.title}</td></tr>
+                                    <tr><td>Описание</td><td>{project.comment}</td></tr>
+                                    <tr><td>Дедлайн</td><td>{dayjs(project.deadline).format("DD.MM.YYYY")}</td></tr>
+                                    <tr><td>Дата создания</td><td>{dayjs(project.created_date).format("DD.MM.YYYY")}</td></tr>
+                                    <tr><td>Дата выполнения</td><td>{project.done_at ? dayjs(project.done_at).format("DD.MM.YYYY") : "-"}</td></tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card">
+                        <div className="card-header">
+                            <h3>Статус задачи</h3>
+                        </div>
+                        <div className="card-body">
+                            {(() => {
+                                if (project.done_at) return <>Задача закрыта</>
+                                if (user.is_boss) return <ProjectCloser task_id={project.id} />
+                                return <>В работе</>
+                            })()}
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card">
+                        <div className="card-header">
+                            <h3>Чат</h3>
+                        </div>
+                        <div className="card-body">
+                            <MessageForm project_id={project.id} />
+                            <Chat messages={messages} essense_type="project" essense_id={id} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </>
 }
