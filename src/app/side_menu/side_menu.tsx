0fc98540f4d4f650d112d5b70feb1f7acaad5d22
Yeminfu <yeminfu4319@gmail.com>
2023-10-20 15:38:16 +1000

    мелочи

--- src/app/side_menu/side_menu.tsx
+++ src/app/side_menu/side_menu.tsx
@@ -74,7 +74,7 @@ export default async function SideMenu() {
         {!user.is_manager ? null : <div className="shadow mb-3">
             <h5 className="m-0">Задачи</h5>
             <ul className="list-group">
-                <li><Link href="/projects/create" className="list-group-item text-nowrap">Создать проект</Link></li>
+                <li><Link href="/projects/create" className="list-group-item text-nowrap">Создать задачу</Link></li>
                 <li><Link href="/projects/get" className="list-group-item text-nowrap">Список проектов</Link></li>
             </ul>
         </div>}
