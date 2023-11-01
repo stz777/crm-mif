e666ca3e42c42155e41ab3a21cb42896c7ec9b76
Yeminfu <yeminfu4319@gmail.com>
2023-11-01 16:27:32 +1000

    add leads to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,22 @@
-// import path from "path"
-// import Image from "next/image"
-import bg from "../../public/mp_background.jpg"
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { getUserByToken } from "./components/getUserByToken";
+import { getLeads } from "./leads/get/getLeadsFn";
+import Filter from "./purchasing_tasks/get/filter";
+import Client from "./leads/get/client";
 
+export default async function Home(props: any) {
+  const auth = cookies().get('auth');
+  if (!auth?.value) return redirect("/");
+  const user = await getUserByToken(auth?.value);
+  if (!user) return redirect("/");
 
-export default function Home() {
-  return (
-    <main>
-      {/* <img src={bg.src} alt="" />
-      <Image
-        src={'/'}
-        // className={styles.image}
-        alt="image"
-        layout="fill"
-      /> */}
-      <div
-        className=""
-        style={{
-          background: `url(${bg.src})`,
-          height: "100%"
-        }}
-      ></div>
-      {/* <img src={bg} alt="" /> */}
-    </main>
-  )
+  const { searchParams } = props;
+  const leads = await getLeads(searchParams);
+  if (!leads?.length) return <>нет заказов</>
+  return <>
+    <h1>Заказы</h1>
+    <Filter searchParams={searchParams} />
+    <Client leads={leads} is_manager={!!user?.is_manager} is_boss={!!user.is_boss} searchParams={searchParams} />
+  </>
 }
