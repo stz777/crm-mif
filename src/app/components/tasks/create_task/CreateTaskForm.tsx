ea56f9e015cafc8bb27a2d7ef3335a171e02aa76
Yeminfu <yeminfu4319@gmail.com>
2023-11-15 18:59:42 +1000

    create tasks/create/route.ts

--- src/app/components/tasks/create_task/CreateTaskForm.tsx
+++ src/app/components/tasks/create_task/CreateTaskForm.tsx
@@ -1,25 +1,60 @@
 "use client"
 
+import { useStore } from "effector-react";
 import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+
 type FormValues = {
-    description: string
-   
+    description: string,
+    image: any
 };
 
 export default function CreateTaskForm() {
     const { register, handleSubmit, control, reset } = useForm<FormValues>();
 
+    const [show, setShow] = useState(false);
 
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     return <>
-        <form
-            onSubmit={handleSubmit(e => onSubmit(e, reset))}
-            style={{ maxWidth: "1000px" }}
-        >
-            <input className="form-control" placeholder="Введите номер телефона" {...register(`description`, { required: true })} autoComplete="off" />
-        </form>
+        <>
+            <button className="btn btn-sm btn-outline-dark"
+                onClick={handleShow}
+            >Создать задачу</button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Создаем задачу</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <form
+                        onSubmit={handleSubmit(e => onSubmit(e, reset))}
+                        style={{ maxWidth: "1000px" }}
+                    >
+                        <div>
+                            <textarea className="form-control" rows={3} placeholder="Описание задачи" {...register(`description`, { required: true })} autoComplete="off" />
+                            <input type="file" {...register("image"/* , { required: true } */)} />
+                        </div>
+                        <button className="btn-btn-sm">Сохранить</button>
+                    </form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Отмена
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Сохранить
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+
     </>;
 }
 
@@ -30,20 +65,22 @@ const onSubmit = (data: any, resetForm: any) => {
 
     for (const key in data) {
         const element = data[key];
-        if (key === "phones") {
-            formdata.append("phones", JSON.stringify(element));
+        if (key === "description") {
+            formdata.append("description", element);
             continue;
         }
-        if (key === "emails") {
-            formdata.append("emails", JSON.stringify(element));
-            continue;
-        }
-        if (key === "telegram") {
-            formdata.append("telegram", JSON.stringify(element));
-            continue;
-        }
+        // if (key === "emails") {
+        //     formdata.append("emails", JSON.stringify(element));
+        //     continue;
+        // }
+        // if (key === "telegram") {
+        //     formdata.append("telegram", JSON.stringify(element));
+        //     continue;
+        // }
 
         if (key === "image") {
+            console.log('image');
+            
             for (let i = 0; i < element.length; i++) {
                 formdata.append('images', element[i]);
             }
@@ -53,23 +90,23 @@ const onSubmit = (data: any, resetForm: any) => {
         formdata.append(key, element);
     }
 
-    if (!data?.phones?.length) {
-        toast.error('Нужно заполнить поле "телефон"');
-        return;
-    }
+    // if (!data?.phones?.length) {
+    //     toast.error('Нужно заполнить поле "телефон"');
+    //     return;
+    // }
 
-    if (
-        (data.deadline || data.description || data.sum)
-        &&
-        !(data.deadline && data.description && data.sum)
-    ) {
-        toast.error('Чтобы создать заказ, нужно заполнить все поля');
-        return;
-    }
+    // if (
+    //     (data.deadline || data.description || data.sum)
+    //     &&
+    //     !(data.deadline && data.description && data.sum)
+    // ) {
+    //     toast.error('Чтобы создать заказ, нужно заполнить все поля');
+    //     return;
+    // }
 
     fetch(
-        String(process.env.TASK_MANAGES_URL) + "/api/tasks/create",
-        // "/api/clients/create",
+        "/api/tasks/create",
+        // String(process.env.TASK_MANAGES_URL) + "/api/tasks/create",
         {
             method: "POST",
             body: formdata
