3a62393a73ee9e4b3cf102d835d2236fd1894b63
Yeminfu <yeminfu4319@gmail.com>
2023-12-08 20:48:25 +1000

    up

--- src/components/clients/controlPanel/controlPanel.tsx
+++ src/components/clients/controlPanel/controlPanel.tsx
@@ -1,19 +1,15 @@
 "use client"
 import Image from "next/image"
-import PlusCircleIcon from "./plus_circle.svg"
+
 import './styles.css'; // Подключение вашего CSS файла
 import SearchIcon from "./search.svg"
 import { toast } from "react-toastify";
+import CreateTask from "./components/create-tast/createTask";
 
 export default function ControlPanel() {
     return <div className="d-flex justify-content-between">
         <div className="d-flex">
-            <button className="btn btn-primary me-3"
-                onClick={() => toast.success("Хуяк это сделалось")}
-            >
-                <Image src={PlusCircleIcon} alt="" className="me-2" width={15} height={15} />
-                Создать заказ
-            </button>
+            <CreateTask />
             <div className="d-flex">
                 <input type="text" style={{ width: 250 }} className="form-control" placeholder="Поиск" />
                 <Image src={SearchIcon} alt="" style={{
@@ -21,6 +17,6 @@ export default function ControlPanel() {
                 }} className="me-2" width={15} height={15} />
             </div>
         </div>
-        <button onClick={() => toast.success("Хуяк то сделалось")} className="btn btn-outline-dark float-left">Показать архив</button>
+        <button onClick={() => toast.success("Go GO GO!")} className="btn btn-outline-dark float-left">Показать архив</button>
     </div>
 }
