f28607e41a8560462ee31e9d533a512e3677a95c
Yeminfu <yeminfu4319@gmail.com>
2023-12-23 17:44:08 +1000

    create leadTr

--- src/app/leads/get/Urgency.tsx
+++ src/app/leads/get/Urgency.tsx
@@ -1,15 +1,6 @@
 import dayjs from 'dayjs'
 
 export default function Urgency(props: { deadline: any, done_at: any }) {
-    const {
-        deadline,
-        done_at
-    } = props;
-    console.log({
-        deadline,
-        done_at
-    });
-
     const date1 = dayjs(props.deadline).set("hour", 0).set("minute", 0).add(1, "hours");
     const date2 = dayjs().set("hour", 0).set("minute", 0);
     const diffInDays = date1.diff(date2, 'day');
--- src/app/leads/get/client.tsx
+++ src/app/leads/get/client.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { LeadInterface } from "@/app/components/types/lead";
 import dayjs from 'dayjs'
-import { useEffect, useState } from "react";
+import { JSXElementConstructor, PromiseLikeOfReactNode, ReactElement, ReactNode, ReactPortal, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import Comment from "./Comment";
 import fetchLeads from "./fetchLeads";
@@ -9,9 +9,12 @@ import TableHeader from "./TableHeader";
 import Urgency from "./Urgency";
 import Phone from "./Phone";
 import TotalSum from "./TotalSum";
+import SideModal from "@/components/SideModal/SideModal";
+import { useForm } from "react-hook-form";
 
 export default function Client(props: { leads: LeadInterface[], is_manager: boolean, is_boss: boolean, searchParams: any }) {
-    const [leads, setLeads] = useState(props.leads)
+    const [leads, setLeads] = useState(props.leads);
+
     useEffect(() => {
         let mount = true;
         (async function refreshData() {
@@ -27,33 +30,108 @@ export default function Client(props: { leads: LeadInterface[], is_manager: bool
     }, [leads])
 
     return <>
-        {leads ? <table className="table table-bordered">
-            <TableHeader is_archive={!!props.searchParams?.is_archive} />
-            <tbody>
-                {leads.map(lead => <tr key={lead.id} onClick={() => {
-                    toast('вывод окна деталей заказа');
-                }}>
-                    <td>{lead.id}</td>
-                    <td>{lead.description}</td>
-                    <td><div onClick={e => e.stopPropagation()}><Comment currentText={lead.comment} lead_id={lead.id} /></div></td>
-                    <td>
-                        <div>{lead.clientData.full_name}</div>
-                        <div><Phone phone={String(lead.clientData.meta.find(item => item.data_type === "phone")?.data)} /></div>
-                    </td>
-                    <td>{dayjs(lead.created_date).format("DD.MM.YYYY")}</td>
-                    <td>
-                        {dayjs(lead.deadline).format("DD.MM.YYYY")}
-                        <div><Urgency deadline={lead.deadline} done_at={lead.done_at} /></div>
-                    </td>
-                    {props.is_manager && <td>
-                        <TotalSum payments={lead.payments || []} leadSum={lead.sum} />
-                    </td>}
-                    {props.searchParams?.is_archive && <td>
-                        <span className="text-nowrap">{lead.done_at ? dayjs(lead.done_at).format("DD.MM.YYYY HH:mm") : "нет"}</span>
-                    </td>}
-                </tr>)}
-            </tbody>
-        </table> : <>нет заказов...</>}
+        {leads ? <div>
+            <table className="table table-bordered">
+                <TableHeader is_archive={!!props.searchParams?.is_archive} />
+                <tbody>
+                    {leads.map(lead =>
+                        <LeadTr lead={lead} key={lead.id}>
+                            <td>{lead.id}</td>
+                            <td>{lead.description}</td>
+                            <td><div onClick={e => e.stopPropagation()}><Comment currentText={lead.comment} lead_id={lead.id} /></div></td>
+                            <td>
+                                <div>{lead.clientData.full_name}</div>
+                                <div><Phone phone={String(lead.clientData.meta.find(item => item.data_type === "phone")?.data)} /></div>
+                            </td>
+                            <td>{dayjs(lead.created_date).format("DD.MM.YYYY")}</td>
+                            <td>
+                                {dayjs(lead.deadline).format("DD.MM.YYYY")}
+                                <div><Urgency deadline={lead.deadline} done_at={lead.done_at} /></div>
+                            </td>
+                            {props.is_manager && <td>
+                                <TotalSum payments={lead.payments || []} leadSum={lead.sum} />
+                            </td>}
+                            {props.searchParams?.is_archive && <td>
+                                <span className="text-nowrap">{lead.done_at ? dayjs(lead.done_at).format("DD.MM.YYYY HH:mm") : "нет"}</span>
+                            </td>}
+                        </LeadTr>)}
+                </tbody>
+            </table>
+
+        </div> : <>нет заказов...</>}
     </>
 }
 
+function LeadTr(props: {
+    lead: LeadInterface,
+    children: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined;
+}) {
+    const [is_open, setIsOpen] = useState(false);
+    return <>
+        <tr onClick={() => {
+            setIsOpen(true);
+        }}>
+            {props.children}
+        </tr>
+        <SideModal isOpen={is_open} closeHandle={() => setIsOpen(false)}>
+            <>
+                <LeadDetails lead={props.lead} />
+            </>
+        </SideModal>
+    </>
+}
+
+function LeadDetails(props: { lead: LeadInterface }) {
+    return <>
+        <div className="d-flex align-items-center border-bottom px-4 py-3 ">
+            <div className="h3" >Детали заказа</div>
+            <span className="ms-3 text-secondary" style={{ fontSize: "0.8em" }}>ID: {props.lead.id}</span>
+        </div>
+        <div className="px-4">
+            <div>
+                <Wrapper title="Описание">
+                    {props.lead.description}
+                </Wrapper>
+                <Wrapper title="Статус">
+                    <CommentForm comment={props.lead.comment} />
+                </Wrapper>
+                <Wrapper title="Дата создания">
+                    {dayjs(props.lead.created_date).format("DD.MM.YYYY")}
+                </Wrapper>
+                <Wrapper title="Дедлайн">
+                    <div className="d-flex">
+                        <div className="me-3">{dayjs(props.lead.deadline).format("DD.MM.YYYY")}</div>
+                        <Urgency deadline={props.lead.deadline} done_at={props.lead.done_at} />
+                    </div>
+                </Wrapper>
+                <Wrapper title="Ответственные">
+                    <></>
+                </Wrapper>
+            </div>
+            {/* <pre>{JSON.stringify(props, null, 2)}</pre> */}
+        </div>
+    </>
+}
+
+function CommentForm(props: { comment: string }) {
+    const { register, handleSubmit, reset }: any = useForm<any>({
+        defaultValues: {
+            comment: props.comment
+        }
+    })
+    return <form>
+        <textarea {...register("comment")} className="form-control" placeholder="коммент" />
+    </form>
+}
+
+function Wrapper(props: {
+    title: string;
+    children: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined;
+}) {
+    return <>
+        <div className="d-flex mb-2">
+            <div style={{ width: 220 }}>{props.title}</div>
+            <div>{props.children}</div>
+        </div>
+    </>
+}
