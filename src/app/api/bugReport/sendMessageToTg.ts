1843f82afa1a0e7bf1d9edf0eeddfa9c08d607ad
Yeminfu <yeminfu4319@gmail.com>
2024-01-08 13:06:34 +1000

    zzzzzzz

--- src/app/api/bugReport/sendMessageToTg.ts
+++ src/app/api/bugReport/sendMessageToTg.ts
@@ -1,24 +1,29 @@
-
 const token = process.env.TELEGRAM_BOT_TOKEN;
 
 export async function sendMessageToTg(message: string, chat_id?: string) {
-    const url = `https://api.telegram.org/bot${token}/sendMessage`
-    return await fetch(
-        url,
-        {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            cache: "no-cache",
-            body: JSON.stringify({
-                chat_id: chat_id ? chat_id : "5050441344",
-                text: "from: "+process.env.SERVER + "\n" + message,
-            })
-        }
-    )
-        .then(x => x.json())
-        .then(x => {
-            return x;
-        })
+  const url = `https://api.telegram.org/bot${token}/sendMessage`;
+  return await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    cache: "no-cache",
+    body: JSON.stringify({
+      chat_id: chat_id ? chat_id : "5050441344",
+      text: "from: " + process.env.SERVER + "\n" + message,
+    }),
+  })
+    .then((response) => {
+      console.log("response", response);
+      if (!response.ok) {
+        throw new Error(`Server responded with status ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((x) => {
+      return x;
+    })
+    .catch((err) => {
+      console.log("err #f9f8023", err);
+    });
 }
