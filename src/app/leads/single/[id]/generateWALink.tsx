a5b859215e6e49097ecdab3209b29d4673512479
Yeminfu <yeminfu4319@gmail.com>
2024-01-18 14:00:48 +1000

    phone text-nowrap

--- src/app/leads/single/[id]/generateWALink.tsx
+++ src/app/leads/single/[id]/generateWALink.tsx
@@ -1,9 +1,9 @@
 export function GenerateWALink(props: { phoneNumber: string }) {
     const толькоЦифры = props.phoneNumber.replace(/\D/g, '');
     if (толькоЦифры.length === 11) {
-        return <a className="text-decoration-none fw-bold " href={`https://wa.me/${'7' + толькоЦифры.slice(1)}`}>{addSpacesToPhoneNumber(props.phoneNumber)} </a>;
+        return <a className="text-decoration-none fw-bold text-nowrap" href={`https://wa.me/${'7' + толькоЦифры.slice(1)}`}>{addSpacesToPhoneNumber(props.phoneNumber)} </a>;
     } else if (толькоЦифры.length === 10) {
-        return <a className="text-decoration-none fw-bold " href={`https://wa.me/${'7' + толькоЦифры.slice(1)}`}> {addSpacesToPhoneNumber(props.phoneNumber)} </a>;
+        return <a className="text-decoration-none fw-bold text-nowrap" href={`https://wa.me/${'7' + толькоЦифры.slice(1)}`}> {addSpacesToPhoneNumber(props.phoneNumber)} </a>;
     } else {
         return <>Некорректный номер телефона {props.phoneNumber} {JSON.stringify(props.phoneNumber)}</>;
     }
