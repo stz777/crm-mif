069eefb1119bb6520aa86227fb4db00dea990bd7
Yeminfu <yeminfu4319@gmail.com>
2024-01-18 13:34:06 +1000

    fix phone view

--- src/app/leads/single/[id]/generateWALink.tsx
+++ src/app/leads/single/[id]/generateWALink.tsx
@@ -1,10 +1,16 @@
 export function GenerateWALink(props: { phoneNumber: string }) {
     const толькоЦифры = props.phoneNumber.replace(/\D/g, '');
     if (толькоЦифры.length === 11) {
-        return <a href={`https://wa.me/${'7' + толькоЦифры.slice(1)}`}> {props.phoneNumber} </a>;
+        return <a className="text-decoration-none fw-bold " href={`https://wa.me/${'7' + толькоЦифры.slice(1)}`}>{addSpacesToPhoneNumber(props.phoneNumber)} </a>;
     } else if (толькоЦифры.length === 10) {
-        return <a href={`https://wa.me/${'7' + толькоЦифры.slice(1)}`}> {props.phoneNumber} </a>;
+        return <a className="text-decoration-none fw-bold " href={`https://wa.me/${'7' + толькоЦифры.slice(1)}`}> {addSpacesToPhoneNumber(props.phoneNumber)} </a>;
     } else {
         return <>Некорректный номер телефона {props.phoneNumber} {JSON.stringify(props.phoneNumber)}</>;
     }
+}
+
+function addSpacesToPhoneNumber(phoneNumber: string) {
+    phoneNumber = phoneNumber.replace(/\D/g, '');
+    phoneNumber = phoneNumber.replace(/(\d{1})(\d{3})(\d{3})(\d{2})(\d{2})/, '+$1 $2 $3 $4 $5');
+    return phoneNumber;
 }
