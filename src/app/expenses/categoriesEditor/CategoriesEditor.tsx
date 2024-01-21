e9545efa7e66f009b5a769c45655eeda79d70c9b
Yeminfu <yeminfu4319@gmail.com>
2024-01-21 12:49:58 +1000

    refactoring

--- src/app/expenses/categoriesEditor/CategoriesEditor.tsx
+++ src/app/expenses/categoriesEditor/CategoriesEditor.tsx
@@ -3,15 +3,11 @@
 import SideModal from "@/components/SideModal/SideModal";
 import { ExpensesCategoryInterface } from "@/types/expenses/expensesCategoryInterface";
 import { useEffect, useState } from "react"
-import { useForm } from "react-hook-form";
 import CreateCategoryForm from "./CreateCategoryForm";
-import { toast } from "react-toastify";
+import CategoryNameEditor from "./CategoryNameEditor";
 
 export default function CategoriesEditor(props: { expensesCategories: ExpensesCategoryInterface[] }) {
     const [modalIsOpen, setModalIsOpen] = useState(false);
-    useEffect(() => {
-
-    }, [])
     return <>
         <button className="btn btn-outline-dark" onClick={() => setModalIsOpen(true)}>Создание/редактирование категорий</button>
         <SideModal isOpen={modalIsOpen} closeHandle={() => setModalIsOpen(false)}>
@@ -34,80 +30,4 @@ export default function CategoriesEditor(props: { expensesCategories: ExpensesCa
             </>
         </SideModal>
     </>
-}
-
-function CategoryNameEditor(props: { expensesCategoryId: number, expensesCategoryName: string }) {
-    const { register, handleSubmit, control, reset, setValue, getValues } = useForm<any>({
-        defaultValues: {
-            id: props.expensesCategoryId,
-            name: props.expensesCategoryName,
-        }
-    });
-    const [isChanged, setIsChanged] = useState(false);
-
-
-    useEffect(() => {
-        setIsChanged(false);
-    }, [props])
-
-    return <>
-        <form
-            onSubmit={handleSubmit(() => null)}
-            style={{ maxWidth: "1000px" }}
-        >
-            <div className="d-flex mb-3">
-                <input type="text" className="form-control"
-                    {...register(`name`, {
-                        onChange: (e: any) => {
-                            const newString = e.target.value;
-                            console.log('newString', newString, props.expensesCategoryName);
-                            if (newString !== props.expensesCategoryName) {
-                                setIsChanged(true);
-                            } else {
-                                setIsChanged(false);
-                            }
-                        }
-                    })}
-                    autoComplete="off" />
-
-                {isChanged && <>
-
-                    <button className="btn btn-sm btn-outline-success" onClick={async () => {
-                        const updated = await fetchUpdateExpensesCategoryName(props.expensesCategoryId, getValues('name'))
-                        if (updated) {
-                            toast.success('Название категории изменено');
-                        }
-
-                    }}>изменить</button>
-
-                    <button className="btn btn-sm btn-outline-danger" onClick={() => {
-                        reset();
-                        setIsChanged(false);
-                    }}>отменить</button>
-
-                </>}
-
-            </div>
-
-        </form>
-        {/* <div className="p-2 border mb-3">{props.expensesCategoryName} ({props.expensesCategoryId})</div> */}
-    </>
-}
-
-function fetchUpdateExpensesCategoryName(category_id: number, category_name: string) {
-    return fetch("/api/expenses-categories/edit-name",
-        {
-            method: "POST",
-            body: JSON.stringify({
-                category_id,
-                category_name
-            })
-        })
-        .then(x => x.json())
-        .then(x => {
-            console.log('xxx', x);
-
-            return x.success;
-        })
-
 }
