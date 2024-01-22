aa1b9798c5886e695f2410da384823076d0c37c8
Yeminfu <yeminfu4319@gmail.com>
2024-01-22 18:41:01 +1000

    update sidemenu

---
+++ src/app/side_menu/client.tsx
@@ -0,0 +1,31 @@
+"use client"
+import Link from "next/link"
+import images from "./icons/Images"
+import { useState } from "react"
+
+export default function Client(props: { currentPath: string }) {
+    const [currentPath, setCurrentPath] = useState(props.currentPath);
+    return <>
+        {menuItems.map((item, i) =>
+            <div key={i} className={`list-group-item ${item.link === currentPath && "list-group-item-light"}`}>
+                <div >
+                    <Link href={item.link} className="text-decoration-none text-dark d-block py-3 px-3 d-flex flex-nowrap"
+                        onClick={() => setCurrentPath(item.link)}
+                    >{item.icon} <span className="ms-1">{item.title}</span></Link>
+                </div>
+            </div>)
+        }
+    </>
+}
+
+
+const menuItems: { title: string, icon: any, link: string, }[] = [
+    { title: "Заказы", icon: images.leads, link: "/" },
+    { title: "Задачи", icon: images.tasks, link: "/tasks" },
+    { title: "Расходы", icon: images.expenses, link: "/expenses" },
+    { title: "Склад", icon: images.stock, link: "/stock" },
+    { title: "Поставщики", icon: images.suppliers, link: "/suppliers" },
+    { title: "Клиенты", icon: images.clients, link: "/clients" },
+    { title: "Сотрудники", icon: images.employees, link: "/employees" },
+    { title: "Отчеты", icon: images.report, link: "/report" },
+]
--- src/app/side_menu/side_menu.tsx
+++ src/app/side_menu/side_menu.tsx
@@ -1,31 +1,16 @@
-import Link from "next/link";
-import images from "./icons/Images"
 import { cookies } from "next/headers";
 import { getUserByToken } from "../components/getUserByToken";
+import { headers } from "next/headers";
+import Client from "./client";
 
 export default async function SideMenu() {
+    const headersList = headers();
+    const currentPath = String(headersList.get("x-invoke-path"));
     const auth = cookies().get('auth');
     if (!auth?.value) return;
     const user = await getUserByToken(auth?.value);
     if (!user) return null;
     return <div>
-        {menuItems.map((item, i) =>
-            <div key={i} className={`list-group-item ${!i && "list-group-item-light"}`}>
-                <div >
-                    <Link href={item.link} className="text-decoration-none text-dark d-block py-3 px-3 d-flex flex-nowrap">{item.icon} <span className="ms-1">{item.title}</span></Link>
-                </div>
-            </div>)
-        }
+        <Client currentPath={currentPath} />
     </div>
 }
-
-const menuItems: { title: string, icon: any, link: string, }[] = [
-    { title: "Заказы", icon: images.leads, link: "/" },
-    { title: "Задачи", icon: images.tasks, link: "/tasks" },
-    { title: "Расходы", icon: images.expenses, link: "/expenses" },
-    { title: "Склад", icon: images.stock, link: "/stock" },
-    { title: "Поставщики", icon: images.suppliers, link: "/suppliers" },
-    { title: "Клиенты", icon: images.clients, link: "/clients" },
-    { title: "Сотрудники", icon: images.employees, link: "/employees" },
-    { title: "Отчеты", icon: images.report, link: "/report" },
-]
