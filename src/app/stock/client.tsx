b3b8c37e8bff2b9536e1959013fa981aef0497e6
Yeminfu <yeminfu4319@gmail.com>
2024-02-01 15:43:52 +1000

    create material creator in client side

--- src/app/stock/client.tsx
+++ src/app/stock/client.tsx
@@ -4,6 +4,9 @@ import SideModal from "@/components/SideModal/SideModal";
 import { $componentState } from "@/components/clients/controlPanel/components/create-lead/store/componentState";
 import { $modalIsOpen, setModalIsOpen } from "@/components/clients/controlPanel/components/create-lead/store/modalState";
 import { useStore } from "effector-react";
+import { JSXElementConstructor, PromiseLikeOfReactNode, ReactElement, ReactNode, ReactPortal, useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
 
 export default function Client() {
     return <>
@@ -54,29 +57,83 @@ export default function Client() {
 }
 
 function CreateMaterial() {
-    const modalIsOpen = useStore($modalIsOpen);
-    const componentState = useStore($componentState);
-    return <>
-        <button className="btn btn-primary me-3" onClick={() => { setModalIsOpen(true); }} >
-            {/* <Image src={PlusCircleIcon} alt="" className="me-2" width={15} height={15} /> */}
-            Создать заказ
-        </button>
+    // const modalIsOpen = useStore($modalIsOpen);
+    const [isOpen, setIsOpen] = useState(false);
 
-        <SideModal isOpen={modalIsOpen} closeHandle={() => setModalIsOpen(false)}>
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+    } = useForm<any>();
+
+    function onSubmit(values: any) {
+        fetch(
+            "/api/stock/create-material",
+            {
+                method: "post",
+                body: JSON.stringify(values)
+            }
+        )
+            .then(x => x.json())
+            .then(x => {
+                if (x.success) {
+                    toast.success("Категория создана");
+                } else {
+                    toast.error("Что-то пошло не так #ff5f84");
+                }
+            })
+            .catch(err => {
+                console.log('error', err);
+                toast.error("Что-то пошло не так #fjf884")
+            })
+    }
+    return <>
+        <button className="btn btn-primary me-3" onClick={() => { setIsOpen(true); }} >
+            Добавить материал
+        </button>
+        <SideModal isOpen={isOpen} closeHandle={() => setIsOpen(false)}>
             <div className="border-bottom px-4 py-3 h3" >
-                {(() => {
-                    if (["default", "client_selected"].includes(componentState)) return <>Создать заказ</>
-                    if (["client_not_selected"].includes(componentState)) return <>Создать клиента</>
-                })()}
+                Добавить материал
             </div>
             <div className="px-4">
-                {/* {(() => {
-                    if (componentState === "default") return <><SearchClient /></>
-                    if (componentState === "client_selected") return <><CreateLeadForm /></>
-                    if (componentState === "client_not_selected") return <><CreateClientForm /></>
-                })()} */}
+                <div>
+                    <form onSubmit={handleSubmit(x => {
+                        onSubmit(x);
+
+                    })}>
+                        <Wrapper title="Наименование">
+                            <input {...register("name", { required: true })} className="form-control" autoComplete="off" />
+                        </Wrapper>
+                        <Wrapper title="Количество">
+                            <input {...register("count", {
+                                required: true,
+                                pattern: /^[0-9]+$/i
+                            })} className="form-control" autoComplete="off" />
+                        </Wrapper>
+                        <div className="d-flex">
+                            <button className="btn btn-sm btn-outline-dark">Сохранить</button>
+                            <div className="btn btn-sm btn-outline-danger ms-2" onClick={() => {
+                                setIsOpen(false);
+                                reset();
+                            }}>отмена</div>
+                        </div>
+                    </form>
+                </div>
             </div>
         </SideModal>
+    </>
+}
 
+
+function Wrapper(props: {
+    title: string;
+    children: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined;
+}) {
+    return <>
+        <div className="d-flex mb-3">
+            <div style={{ width: 220 }}>{props.title}</div>
+            <div>{props.children}</div>
+        </div>
     </>
 }
