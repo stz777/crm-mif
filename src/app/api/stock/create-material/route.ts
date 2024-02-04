2020b5c0b9eca3ff93a85ade8f80f673507bdf88
Yeminfu <yeminfu4319@gmail.com>
2024-02-04 20:08:57 +1000

    fix bug

--- src/app/api/stock/create-material/route.ts
+++ src/app/api/stock/create-material/route.ts
@@ -1,8 +1,16 @@
+import { getUserByToken } from "@/app/components/getUserByToken";
 import insertEntryToHistoryDB from "@/app/db/stock/insertEntryToHistoryDB";
 import insertMaterialToStockDB from "@/app/db/stock/insertMaterialToStockDB";
+import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
+  const auth = cookies().get("auth");
+  if (!auth?.value) return new Response("Кто ты", { status: 401 });
+  const user = await getUserByToken(auth?.value);
+  if (!user) return new Response("Кто ты", { status: 401 });
+  if (!user.is_boss) return new Response("Кто ты", { status: 401 });
+
   const { count, material } = await request.json();
   const materialId = await insertMaterialToStockDB(material, count);
   if (!materialId) {
@@ -10,9 +18,11 @@ export async function POST(request: NextRequest) {
       success: false,
     });
   }
+
   const historyIsUpdated = await insertEntryToHistoryDB(
     materialId,
     count,
+    user.id,
     1,
     "Новый материал"
   );
