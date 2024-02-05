cd9704aba402f97513528d061cd7ad51f9ee0e9d
Yeminfu <yeminfu4319@gmail.com>
2024-02-05 23:26:24 +1000

    add PageTmp to stock history page

--- src/app/stock/history/client.tsx
+++ src/app/stock/history/client.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import fetchStockHistory from "./fetchStockHistory";
 import dayjs from "dayjs";
 import Link from "next/link";
+import PageTmp from "@/app/ui/tmp/page/PageTmp";
 
 export default function Client(props: {
     history: StockHistory[]
@@ -27,32 +28,37 @@ export default function Client(props: {
     }, [props.history])
 
     return <>
-        <h1>Склад (История)</h1>
-        <div className="d-flex justify-content-between">
-            <div></div>
-            <Link href={"/stock"} className="btn btn-outline-dark">Скрыть историю</Link>
-        </div>
-        <div>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>Наименование</th>
-                        <th>Сотрудник</th>
-                        <th>Дата</th>
-                        <th>Тип операции</th>
-                        <th>Кол-во штук</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {stockHistory.map(historyItem => <tr key={historyItem.id}>
-                        <td>{historyItem.material_name}</td>
-                        <td>{historyItem.username}</td>
-                        <td>{dayjs(historyItem.created_date).format("DD.MM.YYYY")}</td>
-                        <td>{historyItem.is_adjunction ? <span className="text-success">Пополнение</span> : <span className="text-danger">Списание</span>}</td>
-                        <td><span className={`text-${historyItem.is_adjunction ? "success" : "danger"}`}>{historyItem.is_adjunction ? "+" : "-"}{historyItem.count}</span></td>
-                    </tr>)}
-                </tbody>
-            </table>
-        </div>
+        <PageTmp
+            title="Склад (История)"
+            filter={<div className="d-flex justify-content-between">
+                <div></div>
+                <Link href={"/stock"} className="btn btn-outline-dark">Скрыть историю</Link>
+            </div>}
+        >
+            <div>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Наименование</th>
+                            <th>Сотрудник</th>
+                            <th>Дата</th>
+                            <th>Тип операции</th>
+                            <th>Кол-во штук</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {stockHistory.map(historyItem => <tr key={historyItem.id}>
+                            <td>{historyItem.material_name}</td>
+                            <td>{historyItem.username}</td>
+                            <td>{dayjs(historyItem.created_date).format("DD.MM.YYYY")}</td>
+                            <td>{historyItem.is_adjunction ? <span className="text-success">Пополнение</span> : <span className="text-danger">Списание</span>}</td>
+                            <td><span className={`text-${historyItem.is_adjunction ? "success" : "danger"}`}>{historyItem.is_adjunction ? "+" : "-"}{historyItem.count}</span></td>
+                        </tr>)}
+                    </tbody>
+                </table>
+            </div>
+        </PageTmp>
+
+
     </>
 }
