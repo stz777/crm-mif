e0c21fd9c8fa6955604b514e03279872296788ed
Yeminfu <yeminfu4319@gmail.com>
2024-02-25 23:20:59 +1000

    add archive btn

--- src/app/tasks/client.tsx
+++ src/app/tasks/client.tsx
@@ -7,6 +7,7 @@ import { SearchInterface } from "./types";
 import { useEffect, useState } from "react";
 import fetchGetTaskData from "./fetchGetTaskData";
 import TaskStatus from "./TaskStatus";
+import querystring from "querystring";
 
 export default function Client(props: { tasks: TaskFromDBInterface[], searchParams: SearchInterface }) {
 
@@ -29,12 +30,29 @@ export default function Client(props: { tasks: TaskFromDBInterface[], searchPara
     }, []);
 
     return <>
-        <div className="d-flex">
-            <div>
+        <div className="d-flex justify-content-between">
+            <div className="d-flex">
                 <CreateTaskForm />
+                <Filter searchParams={props.searchParams} />
             </div>
             <div>
-                <Filter searchParams={props.searchParams} />
+                <button
+                    onClick={() => {
+                        if (props.searchParams.is_archive) {
+                            delete props.searchParams.is_archive;
+                        } else {
+                            props.searchParams.is_archive = "true";
+                        }
+                        const { pathname, origin } = window.location;
+                        const qs = querystring.encode(((): any => props.searchParams)());
+                        // const linkParts = [pathname, `?${qs}`];
+                        const link = `${origin}/${pathname}?${qs}`;
+                        window.location.href = link
+                    }}
+                    className="btn btn-outline-dark float-left"
+                >
+                    {props.searchParams.is_archive ? "скрыть" : "показать"} архив
+                </button>
             </div>
         </div>
         <table className="table w-100">
