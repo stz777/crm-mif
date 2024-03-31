043f8447040d55b0ec603d56f02731bae7e813cf
Yeminfu <yeminfu4319@gmail.com>
2024-03-31 12:06:16 +1000

    rebuild

--- src/components/clients/controlPanel/components/create-lead/CreateClientForm.tsx
+++ src/components/clients/controlPanel/components/create-lead/CreateClientForm.tsx
@@ -1,17 +1,17 @@
 import FieldWrapper from "@/app/ui/form/fieldWrapper";
 import LeadFields from "@/app/clients/create/createClientForm/leadFields";
 import ClientFields from "@/app/clients/create/createClientForm/clientFields";
-import { toast } from "react-toastify";
 import { useForm, useFieldArray } from "react-hook-form";
 import { useEffect } from "react";
 import { useStore } from "effector-react";
 import { $insertedPhone } from "./store/insertedPhone";
-import { reset } from "./store/modalState";
 import { FaTrash } from "react-icons/fa";
+import onSubmit from "./onsubmit";
 
 export default function CreateClientForm() {
     const insertedPhone = useStore($insertedPhone);
     const { register, handleSubmit, control, reset, watch, setValue, resetField } = useForm<any>();
+
     const { fields: phonesFields, append: appendPhone, remove: removePhone } = useFieldArray({
         control,
         name: "phones",
@@ -27,11 +27,10 @@ export default function CreateClientForm() {
 
     const inputValue: any = watch('image');
 
-
-
     useEffect(() => {
         appendPhone({ phone: insertedPhone })
-    }, [insertedPhone])
+    }, [insertedPhone]);
+
     return (
         <form
             onSubmit={handleSubmit((e: any) => onSubmit(e, reset))}
@@ -82,153 +81,4 @@ export default function CreateClientForm() {
             <button className="btn btn-sm btn-outline-dark">Сохранить</button>
         </form>
     );
-}
-
-const onSubmit = (data: any, resetForm: any) => {
-    if (!data.phones?.length) {
-        toast.error('Нужно заполнить поле "телефон"');
-        return;
-    } else {
-
-        for (let index = 0; index < data.phones.length; index++) {
-            const { phone } = data.phones[index];//
-            const numbers = phone.replace(/[^0-9]/igm,"");
-            console.log('phone', [phone, phone.length, numbers, numbers.length]);
-            if(numbers.length<10){
-                toast.error('Нужно корректно заполнить поле "телефон"');
-                return;
-            }
-        }
-    }
-
-    const formdata = new FormData();
-
-    for (const key in data) {
-        const element = data[key];
-        if (key === "phones") {
-            const upgradedPhones = element.map((x: any) => ({ phone: "+7" + x.phone.replace(/[^+0-9]/igm, "") }));
-            formdata.append("phones", JSON.stringify(upgradedPhones));
-            continue;
-        }
-        if (key === "emails") {
-            formdata.append("emails", JSON.stringify(element));
-            continue;
-        }
-        if (key === "telegram") {
-            formdata.append("telegram", JSON.stringify(element));
-            continue;
-        }
-        if (key === "image") {
-            for (let i = 0; i < element.length; i++) {
-                formdata.append('images', element[i]);
-            }
-            continue;
-        }
-
-        formdata.append(key, element);
-    }
-
-    if (!data?.phones?.length) {
-        toast.error('Нужно заполнить поле "телефон"');
-        return;
-    }
-
-    const leadFields = [
-        data?.payment,
-        data?.sum,
-        data?.description,
-        data.deadline
-    ];
-
-    const leadFieldsLength = leadFields.length;
-    const completedLength = leadFields.filter(x => x).length;
-
-    if (completedLength > 0 && leadFieldsLength) {
-        toast.error('Требуется заполнить все поля оплаты или оставить их пустыми"');
-        return;
-    }
-
-    // if(
-
-    // )
-
-    // if (!data?.payment) {
-    //     toast.error('Заполните поле "оплачено"');
-    //     return;
-    // }
-    // if (!data?.sum) {
-    //     toast.error('Заполните поле "сумма заказа"');
-    //     return;
-    // }
-    // if (!data?.description) {
-    //     toast.error('Заполните поле "описание"');
-    //     return;
-    // }
-
-    // if (!data.deadline) {
-    //     toast.error('Заполните поле "дедлайн');
-    //     return;
-    // }
-
-    // if (
-    //     !is_boss
-    //     &&
-    //     ((data.payment || data.image.length)
-    //         &&
-    //         !(data.payment && data.image.length)
-    //     )
-    // ) {
-    //     toast.error('Прикрепите изображение к платежу');
-    //     return;
-    // }
-
-    fetch(
-        "/api/clients/create",
-        {
-            method: "POST",
-            body: formdata
-        }
-    ).then(
-        response => {
-            if (response.ok) {
-                return response.json()
-            } else {
-                throw new Error(response.statusText);
-            }
-        }
-    ).then(data => {
-        if (data.success) {
-            toast.success("Клиент создан");
-            setTimeout(() => {
-                reset();
-            }, 300);
-        } else {
-            toast.error("Что-то пошло не так " + data.error);
-        }
-    })
-        .catch(error => {
-            const statusText = String(error);
-            fetch(
-                `/api/bugReport`,
-                {
-                    method: "POST",
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        text: {
-                            err: "#admDcfck3jm",
-                            data: {
-                                statusText,
-                                values: data
-                            }
-                        }
-                    })
-                }
-            )
-                .then(x => x.json())
-                .then(x => {
-                    console.log(x);
-                })
-        })
 }
---
+++ src/components/clients/controlPanel/components/create-lead/onsubmit.ts
@@ -0,0 +1,112 @@
+import { toast } from "react-toastify";
+import { reset } from "./store/modalState";
+
+export default async function onSubmit(data: any, resetForm: any) {
+  if (!data.phones?.length) {
+    toast.error('Нужно заполнить поле "телефон"');
+    return;
+  } else {
+    for (let index = 0; index < data.phones.length; index++) {
+      const { phone } = data.phones[index]; //
+      const numbers = phone.replace(/[^0-9]/gim, "");
+      console.log("phone", [phone, phone.length, numbers, numbers.length]);
+      if (numbers.length < 10) {
+        toast.error('Нужно корректно заполнить поле "телефон"');
+        return;
+      }
+    }
+  }
+
+  const formdata = new FormData();
+
+  for (const key in data) {
+    const element = data[key];
+    if (key === "phones") {
+      const upgradedPhones = element.map((x: any) => ({
+        phone: "+7" + x.phone.replace(/[^+0-9]/gim, ""),
+      }));
+      formdata.append("phones", JSON.stringify(upgradedPhones));
+      continue;
+    }
+    if (key === "emails") {
+      formdata.append("emails", JSON.stringify(element));
+      continue;
+    }
+    if (key === "telegram") {
+      formdata.append("telegram", JSON.stringify(element));
+      continue;
+    }
+    if (key === "image") {
+      for (let i = 0; i < element.length; i++) {
+        formdata.append("images", element[i]);
+      }
+      continue;
+    }
+
+    formdata.append(key, element);
+  }
+
+  if (!data?.phones?.length) {
+    toast.error('Нужно заполнить поле "телефон"');
+    return;
+  }
+
+  const leadFields = [
+    data?.payment,
+    data?.sum,
+    data?.description,
+    data.deadline,
+  ];
+
+  const leadFieldsLength = leadFields.length;
+  const completedLength = leadFields.filter((x) => x).length;
+
+  if (completedLength > 0 && leadFieldsLength) {
+    toast.error('Требуется заполнить все поля оплаты или оставить их пустыми"');
+    return;
+  }
+
+  fetch("/api/clients/create", {
+    method: "POST",
+    body: formdata,
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(response.statusText);
+      }
+    })
+    .then((data) => {
+      if (data.success) {
+        toast.success("Клиент создан");
+        setTimeout(() => {
+          reset();
+        }, 300);
+      } else {
+        toast.error("Что-то пошло не так " + data.error);
+      }
+    })
+    .catch((error) => {
+      const statusText = String(error);
+      fetch(`/api/bugReport`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          text: {
+            err: "#admDcfck3jm",
+            data: {
+              statusText,
+              values: data,
+            },
+          },
+        }),
+      })
+        .then((x) => x.json())
+        .then((x) => {
+          console.log(x);
+        });
+    });
+}
