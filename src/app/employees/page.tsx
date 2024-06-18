62e8560b6c9e9b2fa95c9c65764071c1fa297cbb
Yeminfu <yeminfu4319@gmail.com>
2024-06-18 19:51:58 +1000

    скрыл сотрудников от менеджера

--- src/app/employees/page.tsx
+++ src/app/employees/page.tsx
@@ -3,8 +3,16 @@ import PageTmp from "../ui/tmp/page/PageTmp";
 import { SearchParamsInterface } from "./types";
 import getEmployeesFromDB from "@/app/db/employees/getEmployeesFromDB";
 import getEmploeeMetaFromDB from "../db/employees/getEmploeeMetaFromDB";
+import { cookies } from "next/headers";
+import { getUserByToken } from "../components/getUserByToken";
 
 export default async function Page(params: { searchParams: SearchParamsInterface }) {
+    const auth = cookies().get('auth');
+    if (!auth?.value) return;
+    const user = await getUserByToken(auth?.value);
+    if (!user) return null;
+    if (!user.is_boss) return null;
+
     const employees = await getEmployeesFromDB({});
     const employeesWithMeta = await Promise.all(
         employees.map(async employee => ({
